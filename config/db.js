b26040fde65dbfb4b613f310fbd522010901a262
dibakar63 <110836157+dibakar63@users.noreply.github.com>
2023-04-18 11:10:55 +0530

    final code

---
+++ config/db.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+dotenv.config()
+const connectDB=async()=>{
+    const conn=await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Mongoose Connected: ${conn.connection.host
+    }`.cyan.underline.bold);
+}
+module.exports=connectDB;
---
+++ index.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const colors=require('colors');
+const cors = require('cors');
+const connectDB=require('./config/db')
+require('dotenv').config();
+const {graphqlHTTP} =require('express-graphql');
+const schema=require('./schema/schema');
+const path=require("path");
+const port=process.env.PORT || 8000; 
+
+const app=express();
+
+//connect to database
+
+connectDB();
+app.use(cors());
+
+
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql:process.env.NODE_ENV='developement'
+}))
+app.listen(port,console.log(`Server running on port ${port}`))
